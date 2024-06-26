a0341cdc05e019cc583c369e8762a33482cf8618
snowma <k.yuuking12@gmail.com>
2024-06-27 01:48:07 +0900

    ☑️publishingの設定

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.serialization)
+    id("maven-publish")
 }
 
 group = "win.snowma"
@@ -13,4 +14,17 @@ repositories {
 dependencies {
     implementation(libs.serialization)
     implementation(libs.kotlin.reflect)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["kotlin"])
+                groupId = "win.snowma"
+                artifactId = "i18nKt"
+                version = "1.0"
+            }
+        }
+    }
 }
